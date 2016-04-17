aaad09d9e847946aaf50efaab355123e5f4f850d
Yaniv Mordekhay <y.mordekhay@gmail.com>
2016-04-17 19:57:59 +0300

    Integrate Jest unit testing

---
+++ __tests__/test_tree.js
@@ -0,0 +1,35 @@
+jest.unmock('../tree');
+import {iterateTree} from '../tree'
+
+describe("Tree iteration tests", () => {
+    const treeData = {
+        name: '1', children: [
+            {name: '1.1', children: []},
+            {name: '1.2', children: [
+                {name: '1.2.1', children: [
+                    {name: '1.2.1.1', children: []}
+                ]}
+            ]},
+            {name: '1.3', children: []}
+        ]
+    }
+    
+    it('tests complete tree iteration', () => {
+        const expectedOrder = ['1', '1.1', '1.2', '1.2.1', '1.2.1.1', '1.3'];
+        
+        iterateTree(treeData, (node) => {
+            expect(node.name).toBe(expectedOrder.shift());
+            return true;
+        })
+    })
+    
+    it('tests partial tree iteration', () => {
+        const expectedOrder = ['1', '1.1', '1.2', '1.3'];
+        
+        iterateTree(treeData, (node) => {
+            expect(node.name).toBe(expectedOrder.shift());
+            return node.name != '1.2';
+        })
+    })
+    
+});
