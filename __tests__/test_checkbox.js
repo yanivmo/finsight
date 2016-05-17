f0316b83a1dbcee2a94731f5afc42f182593c5b0
Yaniv Mordekhay <y.mordekhay@gmail.com>
2016-05-17 19:47:35 +0300

    Test all states of the checkbox component

--- __tests__/test_checkbox.js
+++ __tests__/test_checkbox.js
@@ -7,18 +7,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 
-describe("Checkbox component tests", () => {
-    it('tests indeterminate checkbox', () => {
+describe("Checkbox component tests", function() {
+    const buildAndTestDom = (state) => {
         const checkbox = TestUtils.renderIntoDocument(
-            <Checkbox id="1234" label="Test 1" checkState="indeterminate" />
+            <Checkbox id="1234" label="Test 1" checkState={state} />
         );
         
         const checkboxDom = ReactDOM.findDOMNode(checkbox);
         
-        const inputDom = checkboxDom.children[0];
-        expect(inputDom.tagName).toEqual('INPUT');
+        this.inputDom = checkboxDom.children[0];
+        expect(this.inputDom.tagName).toEqual('INPUT');
         
-        const labelDom = checkboxDom.children[1];
-        expect(labelDom.tagName).toEqual('LABEL');
-    })
+        this.labelDom = checkboxDom.children[1];
+        expect(this.labelDom.tagName).toEqual('LABEL');
+        
+        expect(this.inputDom.id).toEqual(this.labelDom.htmlFor);
+    };
+    
+    it('tests checked checkbox', () => {
+        buildAndTestDom(Checkbox.CHECKED);
+        
+        expect(this.inputDom.indeterminate).toBeFalsy();
+        expect(this.inputDom.checked).toBeTruthy();        
+    });
+
+    it('tests unchecked checkbox', () => {
+        buildAndTestDom(Checkbox.UNCHECKED);
+        
+        expect(this.inputDom.indeterminate).toBeFalsy();
+        expect(this.inputDom.checked).toBeFalsy();        
+    });
+
+    it('tests indeterminate checkbox', () => {
+        buildAndTestDom(Checkbox.INDETERMINATE);
+        
+        expect(this.inputDom.indeterminate).toBeTruthy();
+        expect(this.inputDom.checked).toBeFalsy();
+        
+        TestUtils.Simulate.change(this.inputDom);        
+        expect(this.inputDom.indeterminate).toBeFalsy();
+        expect(this.inputDom.checked).toBeTruthy();
+
+        TestUtils.Simulate.change(this.inputDom);        
+        expect(this.inputDom.indeterminate).toBeFalsy();
+        expect(this.inputDom.checked).toBeFalsy();
+    });
 });
