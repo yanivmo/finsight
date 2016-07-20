45f5812dc22bfab5687d6e638d739b11ad798d4a
Yaniv Mordekhay <y.mordekhay@gmail.com>
2016-07-21 00:57:30 +0300

    Improve checkbox tests

--- __tests__/test_checkbox.js
+++ __tests__/test_checkbox.js
@@ -1,87 +1,79 @@
-'use strict';
-
 jest.unmock('../src/checkbox');
-import {Checkbox} from '../src/checkbox';
+import { Checkbox } from '../src/checkbox';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
 
 import * as CheckboxUtils from '../src/test-utils';
 
 
-describe("Checkbox component tests", function() {
+describe('Checkbox component tests', function () {
     beforeEach(() => {
-        this.container = document.createElement('div'); 
+        jasmine.addMatchers(CheckboxUtils.InputElementMatchers);
+        this.container = document.createElement('div');
     });
-    
+
     const render = (state) => {
         const changeSpy = jest.genMockFunction();
-        
-        const checkbox = ReactDOM.render(
+
+        const checkboxComponent = ReactDOM.render(
             <Checkbox id="1234" label="Test 1" state={state} onChange={changeSpy} />,
             this.container
         );
-        return {objects: getObjects.bind(this, checkbox)};
-    };
-       
-    const getObjects = (checkbox) => {
-        const checkboxDom = ReactDOM.findDOMNode(checkbox);
-        
+        const checkboxDom = ReactDOM.findDOMNode(checkboxComponent);
+
         const inputDom = checkboxDom.children[0];
         expect(inputDom.tagName).toEqual('INPUT');
-        
+
         const labelDom = checkboxDom.children[1];
         expect(labelDom.tagName).toEqual('LABEL');
-        
+
         expect(inputDom.id).toEqual(labelDom.htmlFor);
-        
-        return [checkbox, checkbox.props.onChange];
+
+        return {
+            checkbox: checkboxComponent,
+            changeSpy: checkboxComponent.props.onChange,
+        };
     };
-    
-    beforeEach(() => {
-        jasmine.addMatchers(CheckboxUtils.InputElementMatchers);
-    });
-    
+
     it('tests checked checkbox', () => {
-        const [checkbox, changeSpy] = render(Checkbox.CHECKED).objects();
+        const checkbox = render(Checkbox.CHECKED).checkbox;
         expect(checkbox.inputElement).toBeChecked();
     });
 
     it('tests unchecked checkbox', () => {
-        const [checkbox, changeSpy] = render(Checkbox.UNCHECKED).objects();
+        const checkbox = render(Checkbox.UNCHECKED).checkbox;
         expect(checkbox.inputElement).toBeUnchecked();
     });
 
     it('tests indeterminate checkbox', () => {
-        const [checkbox, changeSpy] = render(Checkbox.INDETERMINATE).objects();
+        const { checkbox, changeSpy } = render(Checkbox.INDETERMINATE);
         expect(checkbox.inputElement).toBeIndeterminate();
 
-        // First click checks the checkbox        
-        CheckboxUtils.clickCheckbox(checkbox);        
+        // First click checks the checkbox
+        CheckboxUtils.clickCheckbox(checkbox);
         expect(checkbox.inputElement).toBeChecked();
 
         // Second click unchecks
-        CheckboxUtils.clickCheckbox(checkbox);        
+        CheckboxUtils.clickCheckbox(checkbox);
         expect(checkbox.inputElement).toBeUnchecked();
-        
+
         // Third click checks again
-        CheckboxUtils.clickCheckbox(checkbox);        
+        CheckboxUtils.clickCheckbox(checkbox);
         expect(checkbox.inputElement).toBeChecked();
-        
+
         // The handler should be called for each click
         expect(changeSpy.mock.calls.length).toBe(3);
         expect(changeSpy.mock.calls[0][0]).toBe(Checkbox.CHECKED);
         expect(changeSpy.mock.calls[1][0]).toBe(Checkbox.UNCHECKED);
         expect(changeSpy.mock.calls[2][0]).toBe(Checkbox.CHECKED);
     });
-    
+
     it('tests checkbox handling of props changes', () => {
-        const [checkbox, changeSpy] = render(Checkbox.UNCHECKED).objects();
+        const checkbox = render(Checkbox.UNCHECKED).checkbox;
         expect(checkbox.inputElement).toBeUnchecked();
-                
-        render(Checkbox.CHECKED);        
+
+        render(Checkbox.CHECKED);
         expect(checkbox.inputElement).toBeChecked();
     });
-
 });
